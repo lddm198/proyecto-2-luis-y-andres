b0fb0e6d156890a7d9ca7de5b0316c69a4c54aea
Luis Diego Delgado M <luisdiego_d@hotmail.com>
2020-11-09 16:32:10 -0600

    Nueva escena para intentar hacer push y nuevos personajes

--- Origin Demo/Assets/Scripts/Escena 2/LecturaDeMouse.cs
+++ Origin Demo/Assets/Scripts/Escena 2/LecturaDeMouse.cs
@@ -6,6 +6,35 @@ using UnityEngine;
 public class LecturaDeMouse : MonoBehaviour
 {
     public List<Vector3> bloqueados;
+
+    private void Start()
+    {
+        bloqueados = new List<Vector3>()
+        {
+        new Vector3(134.9f, 23f, 0f),
+        new Vector3(94.1f, 113.7f, 0f),
+        new Vector3(185.7f, 96.6f, 0f),
+        new Vector3(85.1f, 66.2f, 0f),
+        new Vector3(82.7f, 74.3f, 0f),
+        new Vector3(24.3f, 14f, 0f),
+        new Vector3(62.7f, 64.8f, 0f),
+        new Vector3(64.2f, 73.8f, 0f),
+        new Vector3(75.1f, 75.1f, 0f),
+        new Vector3(75.1f, 66.2f, 0f),
+        new Vector3(154.4f, 137.4f, 0f),
+        new Vector3(185.3f, 138.9f, 0f),
+        new Vector3(215.7f, 127f, 0f),
+        new Vector3(267f, 96.1f, 0f),
+        new Vector3(264.1f, 45.3f, 0f),
+        new Vector3(264.6f, 37.7f, 0f),
+        new Vector3(274.5f, 43.4f, 0f),
+        new Vector3(285.9f, 47.2f, 0f),
+        new Vector3(275f, 34.4f, 0f),
+        new Vector3(283.1f, 34.4f, 0f)
+        };
+    }
+
+    
     
     //public Pathfinding pathfinding = new Pathfinding(20, 10);
     // Update is called once per frame
@@ -18,8 +47,8 @@ public class LecturaDeMouse : MonoBehaviour
 
         if (Input.GetMouseButtonDown(1))
         {
-            bloqueados.Add(UtilsClass.GetMouseWorldPosition());
-            Debug.Log(UtilsClass.GetMouseWorldPosition());
+            //bloqueados.Add(UtilsClass.GetMouseWorldPosition());
+            //Debug.Log(UtilsClass.GetMouseWorldPosition());
         }
     }
 }
